60ca3a68164258bb25ea18bd869d0e6923d3e552
Kristijan Marijic <kikristijan@gmail.com>
2021-08-13 13:52:14 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', [DashboardController::class, 'index']);
+
+Route::group(['prefix' => 'tables'], function(){
+    Route::get('basic-tables', function () { return view('pages.tables.basic-tables'); });
+    Route::get('data-table', function () { return view('pages.tables.data-table'); });
+});
+
+Route::group(['prefix' => 'general'], function(){
+    Route::get('profile', function () { return view('pages.general.profile'); });
+});
+
+Route::group(['prefix' => 'auth'], function(){
+    Route::get('login', function () { return view('pages.auth.login'); });
+    Route::get('register', function () { return view('pages.auth.register'); });
+});
+
+Route::group(['prefix' => 'error'], function(){
+    Route::get('404', function () { return view('pages.error.404'); });
+    Route::get('500', function () { return view('pages.error.500'); });
+});
+
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return "Cache is cleared";
+});
+
+// 404 for undefined routes
+Route::any('/{page?}',function(){
+    return View::make('pages.error.404');
+})->where('page','.*');
