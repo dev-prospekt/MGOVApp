cf973e13cfd550318f3eec25df994ac5765a0d39
dev@prospekt.hr <dev@prospekt.hr>
2021-08-17 11:27:53 +0200

    Models main folder created

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
-        
+
         return view('dashboard', [
             'user' => $user
         ]);
