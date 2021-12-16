7296a42d63e6702ae62af27312ae65d0c4e8efc9
dev-prospekt <dev@prospekt.hr>
2021-12-16 15:01:29 +0100

    json

--- database/seeders/PermissionsDemoSeeder.php
+++ database/seeders/PermissionsDemoSeeder.php
@@ -21,44 +21,47 @@ class PermissionsDemoSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-        Permission::firstOrCreate(['name' => 'edit animal data']);
-        Permission::firstOrCreate(['name' => 'delete animal data']);
-        Permission::firstOrCreate(['name' => 'publish animal data']);
-        Permission::firstOrCreate(['name' => 'unpublish animal data']);
+        // Permission::firstOrCreate(['name' => 'edit animal data']);
+        // Permission::firstOrCreate(['name' => 'delete animal data']);
+        // Permission::firstOrCreate(['name' => 'publish animal data']);
+        // Permission::firstOrCreate(['name' => 'unpublish animal data']);
 
         // create roles and assign existing permissions
-        $role1 = Role::firstOrCreate(['name' => 'Shelter-Admin']);
-        $role1->givePermissionTo('edit animal data');
-        $role1->givePermissionTo('delete animal data');
-        $role1->givePermissionTo('publish animal data');
-        $role1->givePermissionTo('unpublish animal data');
+        // $role1 = Role::firstOrCreate(['name' => 'Shelter-Admin']);
+        // $role1->givePermissionTo('edit animal data');
+        // $role1->givePermissionTo('delete animal data');
+        // $role1->givePermissionTo('publish animal data');
+        // $role1->givePermissionTo('unpublish animal data');
 
 
-        $role2 = Role::firstOrCreate(['name' => 'Super-Admin']);
+        //$role2 = Role::firstOrCreate(['name' => 'Super-Admin']);
         // gets all permissions via Gate::before rule; see AuthServiceProvider
 
-        $role3 = Role::firstOrCreate(['name' => 'Shelter-User']);
+        //$role3 = Role::firstOrCreate(['name' => 'Shelter-User']);
 
         // create demo users
-        $user1 = User::factory()->create([
-            'name' => 'Shelter User',
-            'email' => 'shelter@test.com',
-            'shelter_id' => 2
-        ]);
-        $user1->assignRole($role1);
+        // $user1 = User::create([
+        //     'name' => 'Shelter User',
+        //     'email' => 'shelter@test.com',
+        //     'shelter_id' => 2,
+            
+        // ]);
+        // $user1->assignRole($role1);
 
-        $user2 = User::factory()->create([
+        $user2 = User::create([
             'name' => 'Admin User',
             'email' => 'admin@test.com',
-            'shelter_id' => 1
+            'shelter_id' => 1,
+            'password' =>  bcrypt('adminroot'), // password
+            'remember_token' => Str::random(10),
         ]);
-        $user2->assignRole($role2);
+        //$user2->assignRole($role2);
 
-        $user3 = User::factory()->create([
-            'name' => 'ShelterUser2',
-            'email' => 'shelter2@test.com',
-            'shelter_id' => 3
-        ]);
-        $user3->assignRole($role3);
+        // $user3 = User::factory()->create([
+        //     'name' => 'ShelterUser2',
+        //     'email' => 'shelter2@test.com',
+        //     'shelter_id' => 3
+        // ]);
+        // $user3->assignRole($role3);
     }
 }
--- database/seeders/ShelterSeeder.php
+++ database/seeders/ShelterSeeder.php
@@ -15,7 +15,7 @@ class ShelterSeeder extends Seeder
      */
     public function run()
     {
-        $shelter1 = Shelter::factory()->create([
+        $shelter1 = Shelter::create([
             'name' => 'Aquarium Pula d.o.o.',
             'oib' => 972615522,
             'address' => 'Verudela bb, HR-52100 Pula',
@@ -26,7 +26,7 @@ class ShelterSeeder extends Seeder
         $shelter1->shelterTypes()->attach([1, 2, 3]);
         $shelter1->animalSystemCategory()->attach([1, 3]);
 
-        $shelter2 = Shelter::factory()->create([
+        $shelter2 = Shelter::create([
             'name' => 'AWAP – Udruga za zaštitu divljih životinja',
             'address' => 'Siget 6, HR-10000 Zagreb',
             'address_place' => 'Siget 6',
@@ -37,7 +37,7 @@ class ShelterSeeder extends Seeder
         $shelter2->shelterTypes()->attach([1, 2]);
         $shelter2->animalSystemCategory()->attach([3, 4, 5]);
 
-        $shelter3 = Shelter::factory()->create([
+        $shelter3 = Shelter::create([
             'name' => 'Javna ustanova Nacionalni park Brijuni',
             'address' => 'Brionska 10, HR-52212 Fažana',
             'address_place' => 'Brionska 10',
