143d1a2dd7dc58924d89a9d0944f38a192c21914
dev-prospekt <dev@prospekt.hr>
2022-04-20 13:12:23 +0200

    count animal another shelter

--- resources/views/animal/animal_group/show.blade.php
+++ resources/views/animal/animal_group/show.blade.php
@@ -32,7 +32,7 @@
     </li>
     <li class="nav-item">
         <a class="nav-link" id="anotherShelter-tab" data-toggle="tab" href="#anotherShelter" role="tab" aria-controls="anotherShelter" aria-selected="false">
-            Poslane u drugo oporavilište
+            Poslane u drugo oporavilište ({{ $animal_group->animalAnotherShelter->count() }})
         </a>
     </li>
 </ul>
