9eeabb1c5137d66db90563e84e7d525facc081ae
dev-prospekt <dev@prospekt.hr>
2022-05-04 10:56:00 +0200

    logo link

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sidebar">
   <div class="sidebar-header">
-    <a href="#" class="sidebar-brand font-weight-bold">
+    <a href="{{ url('/') }}" class="sidebar-brand font-weight-bold">
       Mgov<span>App</span>
     </a>
     <div class="sidebar-toggler not-active">
