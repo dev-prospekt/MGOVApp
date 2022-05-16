39d506303a9cfd669f3b0c917f6691cf03e4fb14
dev-prospekt <dev@prospekt.hr>
2022-05-16 09:32:14 +0200

    shelter create

--- routes/web.php
+++ routes/web.php
@@ -43,12 +43,7 @@ Route::group(['middleware' => ['auth']], function () {
     // Logout
     Route::get('logout', [LoginController::class, 'logout']);
 
-    Route::resource('shelter', Shelter\ShelterController::class)->only([
-        'index', 'store', 'show', 'edit', 'update', 'destroy'
-    ]);; // Shelter
-
-    Route::get('shelter-create', 'Shelter\ShelterCreateController@create')->name('shelter.create');
-
+    Route::resource('shelter', Shelter\ShelterController::class); // Shelter
     Route::resource('shelter_legal_staff', Shelter\ShelterLegalStaffController::class);
     Route::resource('shelter_care_staff', Shelter\ShelterCareStaffController::class);
     Route::resource('shelter_vet_staff', Shelter\ShelterVetStaffController::class);
