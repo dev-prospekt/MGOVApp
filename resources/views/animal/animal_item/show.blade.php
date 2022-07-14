1cc72029832b2e658a2d28c274392b9780c6ee57
dev-prospekt <dev@prospekt.hr>
2022-07-14 11:37:22 +0200

    animal_item_show edit variable

--- resources/views/animal/animal_item/show.blade.php
+++ resources/views/animal/animal_item/show.blade.php
@@ -359,7 +359,7 @@
                 <div class="col-md-6">
                   <ul class="list-group list-group-flush">
                     <li class="list-group-item"><span class="text-danger">ZAVRŠENA SKRB</span></li>
-                    <li class="list-group-item">Razlog prestanka skrbi: <span class="text-warning">{{ $animalItem->careEnd->careEndType->name }}</span></li>
+                    <li class="list-group-item">Razlog prestanka skrbi: <span class="text-warning">{{ $animalItem->careEnd->careEndType->name ?? '' }}</span></li>
                     <li class="list-group-item">Trajanje skrbi: <span class="text-warning">{{ $date->start_date->diffInDays($date->end_date) }} dan/a</span></li>
                      
                   </ul>
